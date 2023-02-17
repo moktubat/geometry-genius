af50447c6094048fb8e36a653b46138574e3d751
moktubat <shadhinreja@gmail.com>
2023-02-18 02:23:42 +0600

    displyed data by .innerText

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-console.log("JS file added");
+let count = 0;
+document.getElementById('pentagon-btn').addEventListener('click', function(){
+    count ++;
+    const pentagonTitle = document.getElementById('pentagon-title').innerText;
+    const firstPentagonValue = document.getElementById('pentagon-first-value').innerText;
+    const secondPentagonValue = document.getElementById('pentagon-second-value').innerText;
+
+    console.log(pentagonTitle, firstPentagonValue, secondPentagonValue);
+
+    const areaValue= 0.5 * parseInt(firstPentagonValue) * parseInt(secondPentagonValue);
+
+    const pentagonValue = areaValue;
+    console.log(pentagonValue);
+
+    displayData(pentagonTitle, pentagonValue);
+    
+})
+
+document.getElementById('ellipse-btn').addEventListener('click', function(){
+    count ++;
+    const ellipseTitle = document.getElementById('ellipse-title').innerText;
+    const firstEllipseValue = document.getElementById('ellipse-first-value').innerText;
+    const secondEllipseValue = document.getElementById('ellipse-second-value').innerText;
+
+    console.log(ellipseTitle, firstEllipseValue, secondEllipseValue);
+
+    const areaValue= Math.PI * parseInt(firstEllipseValue) * parseInt(secondEllipseValue);
+
+    const ellipseValue = areaValue.toFixed(2);
+    console.log(ellipseValue);
+
+    displayData(ellipseTitle, ellipseValue);
+    
+})
+
+function displayData(shapeTitle, shapeValue) {
+    const tableContainer = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${count}</td>
+        <td>${shapeTitle}</td>
+        <td>${shapeValue}cm<sup>2</sup></td>
+        <td><button class="btn bg-[#1090D8] border-0">Covert to m<sup>2</sup></button></td>
+    `;
+    tableContainer.appendChild(tr);
+}
