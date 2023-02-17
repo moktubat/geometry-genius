9a56c6c67be7cc86d021d11af1aa37aafc4f1ed2
moktubat <shadhinreja@gmail.com>
2023-02-18 02:47:24 +0600

    displayed data by .value

--- js/index.js
+++ js/index.js
@@ -1,16 +1,55 @@
 let count = 0;
+document.getElementById('triangle-btn').addEventListener('click', function(){
+    count ++;
+    const triangleTitle = document.getElementById('triangle-title').innerText;
+    const firstTriangleValue = document.getElementById('triangle-first-value').value;
+    const secondTriangleValue = document.getElementById('triangle-second-value').value;
+
+    const areaValue= 0.5 * parseInt(firstTriangleValue) * parseInt(secondTriangleValue);
+
+    const triangleValue = areaValue;
+
+    displayData(triangleTitle, triangleValue);
+    
+})
+
+document.getElementById('rectangle-btn').addEventListener('click', function(){
+    count ++;
+    const rectangleTitle = document.getElementById('rectangle-title').innerText;
+    const firstRectangleValue = document.getElementById('rectangle-first-value').value;
+    const secondRectangleValue = document.getElementById('rectangle-second-value').value;
+
+    const areaValue= parseInt(firstRectangleValue) * parseInt(secondRectangleValue);
+
+    const rectangleValue = areaValue;
+
+    displayData(rectangleTitle, rectangleValue);
+    
+})
+
+document.getElementById('parallelogram-btn').addEventListener('click', function(){
+    count ++;
+    const parallelogramTitle = document.getElementById('parallelogram-title').innerText;
+    const firstParallelogramValue = document.getElementById('parallelogram-first-value').value;
+    const secondParallelogramValue = document.getElementById('parallelogram-second-value').value;
+
+    const areaValue= parseInt(firstParallelogramValue) * parseInt(secondParallelogramValue);
+
+    const parallelogramValue = areaValue;
+
+    displayData(parallelogramTitle, parallelogramValue);
+    
+})
+
 document.getElementById('pentagon-btn').addEventListener('click', function(){
     count ++;
     const pentagonTitle = document.getElementById('pentagon-title').innerText;
     const firstPentagonValue = document.getElementById('pentagon-first-value').innerText;
     const secondPentagonValue = document.getElementById('pentagon-second-value').innerText;
 
-    console.log(pentagonTitle, firstPentagonValue, secondPentagonValue);
-
     const areaValue= 0.5 * parseInt(firstPentagonValue) * parseInt(secondPentagonValue);
 
     const pentagonValue = areaValue;
-    console.log(pentagonValue);
 
     displayData(pentagonTitle, pentagonValue);
     
@@ -22,12 +61,9 @@ document.getElementById('ellipse-btn').addEventListener('click', function(){
     const firstEllipseValue = document.getElementById('ellipse-first-value').innerText;
     const secondEllipseValue = document.getElementById('ellipse-second-value').innerText;
 
-    console.log(ellipseTitle, firstEllipseValue, secondEllipseValue);
-
     const areaValue= Math.PI * parseInt(firstEllipseValue) * parseInt(secondEllipseValue);
 
     const ellipseValue = areaValue.toFixed(2);
-    console.log(ellipseValue);
 
     displayData(ellipseTitle, ellipseValue);
     
